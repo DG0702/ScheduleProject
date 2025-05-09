78576137ad17d08669be922ee89e194f20dff15d
DongGeun <kk506kk@naver.com>
2025-05-09 13:35:13 +0900

    ScheduleRepository 인터페이스 메서드 생성

--- src/main/java/com/project/scheduleproject/repository/ScheduleRepository.java
+++ src/main/java/com/project/scheduleproject/repository/ScheduleRepository.java
@@ -1,7 +1,20 @@
 package com.project.scheduleproject.repository;
 
+import com.project.scheduleproject.entity.Schedule;
+
+import java.util.List;
+
 public interface ScheduleRepository {
 
+    Schedule save(Schedule schedule);
+
+    Schedule findById(long id);
+
+    List<Schedule> findAll();
+
+    Schedule update(Schedule schedule);
+
+    String delete(Schedule schedule);
 
 }
 
